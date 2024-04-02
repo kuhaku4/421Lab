405d189c54eb2a1f4659b1f3417154144cc27f46
kuhaku4 <edalmonr@millersville.edu>
2024-04-02 02:44:44 +0000

    Lab6

---
+++ app_api/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config();
+var ctrlBlogs = require('../controllers/blogs');
+var ctrlAuth = require('../controllers/authentication');
+var jwt = require('express-jwt');
+
+var auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
+
+
+router.get('/blogs', ctrlBlogs.blogsList);
+
+router.post('/blogs/add', auth, ctrlBlogs.blogsCreate);
+
+router.get('/blogs/:blogid', ctrlBlogs.blogsReadOne);
+
+router.put('/blogs/:blogid', auth, ctrlBlogs.blogsUpdate);
+
+router.delete('/blogs/:blogid', auth, ctrlBlogs.blogsDelete);
+
+router.post('/register', ctrlAuth.register);
+
+router.post('/login', ctrlAuth.login);
+
+module.exports = router;
