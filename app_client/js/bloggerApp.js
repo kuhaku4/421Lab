db2edc034a603645e4d648e447dea5b38100676e
kuhaku4 <edalmonr@millersville.edu>
2024-04-09 13:40:06 +0000

    Lab8

--- app_client/js/bloggerApp.js
+++ app_client/js/bloggerApp.js
@@ -324,26 +324,3 @@ app.controller('DeleteController', [ '$http', '$routeParams', '$location', 'auth
     $location.path('/blogs'); // Redirect to the blog list if cancel is clicked
   }
 }]);
-
-module.exports = app => {
-
-  app.get('/chess', (req, res) => {
-      res.render('pages/chess.html');
-  });
-
-  app.get('/white', (req, res) => {
-      res.render('game', {
-          color: 'white'
-      });
-  });
-
-  app.get('/black', (req, res) => {
-      if (!games[req.query.code]) {
-          return res.redirect('/?error=invalidCode');
-      }
-
-      res.render('game', {
-          color: 'black'
-      });
-  });
-};
